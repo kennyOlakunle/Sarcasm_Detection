0c6245ed7a95207ddc6e5234e38e451ecdd0a5c1
Kehinde Abe <46799770+kennyOlakunle@users.noreply.github.com>
2024-12-29 16:55:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import joblib
 import json
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from keras.models import load_model, save_model
+from tensorflow.keras.models import load_model, save_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 import numpy as np
@@ -11,10 +10,14 @@ import numpy as np
 # Load the trained model and tokenizer
 @st.cache_resource
 def load_resources():
-    model = tf.keras.models.load_model('new_model.keras')
-    tokenizer = joblib.load('tokenizer.joblib')
-    print(tokenizer)
-    return model, tokenizer
+    try:
+        model = tf.keras.models.load_model('new_model.keras', custom_objects={'custom_activation': tf.nn.relu})
+        tokenizer = joblib.load('tokenizer.joblib')
+        print(tokenizer)
+        return model, tokenizer
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        raise
 
 
 model, tokenizer = load_resources()
