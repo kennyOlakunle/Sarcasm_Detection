bcf993a583aa8389103fa7f1d783826dbf82e3e4
Kehinde Abe <46799770+kennyOlakunle@users.noreply.github.com>
2024-12-30 17:59:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import numpy as np
 @st.cache_resource
 def load_resources():
     try:
-        model = tf.keras.models.load_model('new_model.keras', custom_objects={'custom_activation': tf.nn.relu})
+        model = tf.keras.models.load_model('saved_model_format')
         tokenizer = joblib.load('tokenizer.joblib')
         print(tokenizer)
         return model, tokenizer
